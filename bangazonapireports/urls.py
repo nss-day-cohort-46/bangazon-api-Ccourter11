3b8146bbc89d1ae72d93cc75c67aa84afb9512f8
Ccourter11 <courtercalvin@yahoo.com>
2021-06-03 13:52:08 -0500

    #6 can see all cars and information priced over 999

--- bangazonapireports/urls.py
+++ bangazonapireports/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import userfavorite_list, unpaidorders_list, completedorders_list, inexpensiveproduct_list
+from .views import userfavorite_list, unpaidorders_list, completedorders_list, inexpensiveproduct_list, expensiveproduct_list
 
 urlpatterns = [
     path('reports/userfavorite', userfavorite_list),
     path('reports/unpaidorders', unpaidorders_list),
     path('reports/completedorders', completedorders_list),
     path('reports/inexpensiveproducts', inexpensiveproduct_list),
+    path('reports/expensiveproducts', expensiveproduct_list),
 ]
--- bangazonapireports/views/__init__.py
+++ bangazonapireports/views/__init__.py
@@ -3,3 +3,4 @@ from .users.favoritesbyuser import userfavorite_list
 from .orders.unpaidorders import unpaidorders_list
 from .orders.completedorders import completedorders_list
 from .products.inexpensiveproducts import inexpensiveproduct_list
+from .products.expensiveproducts import expensiveproduct_list
