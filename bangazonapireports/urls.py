567aab25d5bd83a23cbffeb1b623062ee6eb65ea
Ccourter11 <courtercalvin@yahoo.com>
2021-06-03 13:15:52 -0500

    #5 inexpensive products now visible

--- bangazonapireports/urls.py
+++ bangazonapireports/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import userfavorite_list, unpaidorders_list, completedorders_list
+from .views import userfavorite_list, unpaidorders_list, completedorders_list, inexpensiveproduct_list
 
 urlpatterns = [
     path('reports/userfavorite', userfavorite_list),
     path('reports/unpaidorders', unpaidorders_list),
     path('reports/completedorders', completedorders_list),
+    path('reports/inexpensiveproducts', inexpensiveproduct_list),
 ]
--- bangazonapireports/views/__init__.py
+++ bangazonapireports/views/__init__.py
@@ -2,3 +2,4 @@ from .connection import Connection
 from .users.favoritesbyuser import userfavorite_list
 from .orders.unpaidorders import unpaidorders_list
 from .orders.completedorders import completedorders_list
+from .products.inexpensiveproducts import inexpensiveproduct_list
